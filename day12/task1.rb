201d052bfaeca92971c5bb4a7630bd6a8e5469fb
Galen Palmer <palmergs@gmail.com>
2021-12-12 00:39:57 -0500

    part 1

--- day12/task1.rb
+++ day12/task1.rb
@@ -1,1 +1,33 @@
-input = File.read('input.txt')
+input = File.read('input.txt').split(/[\n\r]/)
+map = Hash.new {|h,k| h[k] = [] }
+input.each do |path|
+  lhs, rhs = path.split('-')
+  map[lhs] << rhs unless rhs == 'start' || lhs == 'end'
+  map[rhs] << lhs unless rhs == 'end' || lhs == 'start'
+end
+pp map
+
+require 'set'
+
+def visit map, curr, paths, path = []
+  path << curr
+  puts "PATH = #{ path.inspect }"
+  if curr == 'end'
+    puts "END"
+    paths << path
+    return
+  end
+
+  options = map[curr]
+  puts "OPTIONS: #{ curr } => #{ options.inspect }"
+  options.each do |option|
+    if option != option.downcase || !path.include?(option)
+      visit(map, option, paths, path.clone)
+    end
+  end
+end
+
+paths = []
+visit(map, 'start', paths)
+puts "PATHS = #{ paths.inspect }"
+puts "PATHS = #{ paths.count }"
